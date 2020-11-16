68fa9979b95baaf73980fcc0f41566caf4f8811a
Roosa Kontinen <roosa.kontinen@hotmail.com>
2020-11-16 11:55:51 +0200

    Lisätty kanalle ja dinolle liikkuvuus. Jos dino saa kanan kiinni, kana tuhotaan

---
+++ Assets/Scripts/ChickenMoving.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenMoving : MonoBehaviour
+{
+
+    public GameObject Cube; //The Cube
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //Set speed to chicken
+    void Update()
+    {
+        Cube.transform.position += new Vector3(3f, 0f, 0f);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "chome")
+        {
+            Debug.Log("Paastiin kotiin!");
+            Destroy(col.gameObject);
+        }
+    }
+
+}
+
---
+++ Assets/Scripts/DinoMoving.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoMoving : MonoBehaviour
+{
+
+    public GameObject Cube; //The Cube
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    //Set speed to dino
+    void Update()
+    {
+        Cube.transform.position += new Vector3(2f, 0f, 0f);
+
+    }
+
+    //Check if collision has happened
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Kana")
+        {
+            Debug.Log("Tormays tapahtunut!");
+            Destroy(collision.gameObject);
+        }
+    }
+}
