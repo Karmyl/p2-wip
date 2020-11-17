47b5a254eb4518644ed87c909629b31ef19d7d3f
Roosa Kontinen <roosa.kontinen@hotmail.com>
2020-11-17 21:59:18 +0200

    Mainmenua, Gameover ja asetukset sivua muokattu.

--- Assets/Scripts/MainMenu_BGMusic.cs
+++ Assets/Scripts/MainMenu_BGMusic.cs
@@ -1,16 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SwitchMusicTrigger : MonoBehaviour
+public class MainMenu_BGMusic : MonoBehaviour
 {
-    public AudioClip newTrack;
-    private AudioManager audiomanager;
     // Start is called before the first frame update
     void Start()
     {
-        audiomanager = FindObjectOfType<AudioManager>();
+        
     }
 
     // Update is called once per frame
@@ -18,5 +15,4 @@ public class SwitchMusicTrigger : MonoBehaviour
     {
         
     }
-
 }
