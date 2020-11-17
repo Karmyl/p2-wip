aa6766e56e67229d31c2943eee026204a0d5f97e
Karri Myller <1905311@edu.karelia.fi>
2020-11-18 00:29:40 +0200

    Audio manager taustamusiikeille

---
+++ Assets/Scripts/SwitchMusicTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchMusicTrigger : MonoBehaviour
+{
+    public AudioClip newTrack;
+    private AudioManager audiomanager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiomanager = FindObjectOfType<AudioManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
