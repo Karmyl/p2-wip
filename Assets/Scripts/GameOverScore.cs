20f585aacd04a033b40dc7c8c7f1881757c471ba
Niko <1804194@edu.karelia.fi>
2020-11-23 18:46:38 +0200

    Luotu pisteenlasku scripti GameOver-ruutuun

---
+++ Assets/Scripts/GameOverScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScore : MonoBehaviour
+{
+    private static Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject go = GameObject.Find("CoinsText");
+        if (go)
+        {
+            GameOverScore.text = go.GetComponent<Text>();
+            GameOverScore.text.text = Score.score.ToString();
+        }
+    }
+}
