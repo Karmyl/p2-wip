861d338d8a51eaa8de4783435fbe166e3dc5ed55
Karri Myller <1905311@edu.karelia.fi>
2020-11-24 16:56:52 +0200

    Äänenvoimakkuussäätimen toiminnallisuus toteutettu

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 using System;
 public class AudioManager : MonoBehaviour
 {
-
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private int firstPlayValue;
     public Sound[] sounds;
 
     public AudioSource backgroundMusic;
+    private float musicVolumeValue;
 
+    
     private void Awake()
-    { 
+    {
+        ContinueSettings();
+
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
             s.source.name = s.name;
             s.source.volume = s.volume;
-
         }
+
+        Debug.Log(PlayerPrefs.GetInt("FirstPlay"));
+
     }
 
+    //Keep sound volume settings alive
+    private void ContinueSettings()
+    {
+        musicVolumeValue = PlayerPrefs.GetFloat(BackgroundPref);
+        backgroundMusic.volume = musicVolumeValue;
+    }
+
+    //play sound effect by name
     public void PlaySound(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
@@ -35,16 +51,28 @@ public class AudioManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        if (firstPlayValue == 0)//set values
+        {
+            musicVolumeValue = .5f;
+
+            PlayerPrefs.SetFloat(BackgroundPref, musicVolumeValue);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+
+        }
+        else //get values from playerprefs and set slider
+        {
+            musicVolumeValue = PlayerPrefs.GetFloat(BackgroundPref);
+        }
     }
 
     public void ChangeBackgroundMusic(AudioClip music)
     {
-        if(backgroundMusic.clip.name == music.name)
+        if(backgroundMusic.clip.name == music.name)//Continue without changing music
         {
             return;
-        } else
-        {
-            
+        } else //stop current music and start new bgmusic 
+        {           
             backgroundMusic.Stop();
             backgroundMusic.clip = music;
             if (backgroundMusic.clip.name == "Game_Over_BGMusic")
---
+++ Assets/Scripts/VolumeSettings.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+
+    AudioManager music;
+    AudioSource backgroundMusic;
+    [SerializeField] Slider musicVolumeSlider;
+   
+
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private int firstPlayValue;
+    private float musicVolumeValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    private void Awake()
+    {
+        //Get playerprefs to see if players first play
+        music = FindObjectOfType<AudioManager>().GetComponent<AudioManager>();
+        backgroundMusic = music.backgroundMusic;
+
+        if (firstPlayValue == 0)//set values
+        {
+            musicVolumeValue = backgroundMusic.volume;
+            musicVolumeSlider.value = musicVolumeValue;
+
+            PlayerPrefs.SetFloat(BackgroundPref, musicVolumeValue);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+
+            SaveSoundSettings();
+        }
+        else //get values from playerprefs and set slider
+        {
+            musicVolumeValue = PlayerPrefs.GetFloat(BackgroundPref);
+            musicVolumeSlider.value = musicVolumeValue;
+
+            
+        }
+
+        Debug.Log(music.name);
+    }
+    //save sound values
+    public void SaveSoundSettings()
+    {
+        PlayerPrefs.SetFloat(BackgroundPref, musicVolumeSlider.value);
+    }
+
+    //if player loses focus in the game -> save settings
+    private void OnApplicationFocus(bool focus)
+    {
+        if(!focus)
+        {
+            SaveSoundSettings();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        backgroundMusic.volume = musicVolumeSlider.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        backgroundMusic.volume = musicVolumeSlider.value;
+
+        foreach (Sound s in music.sounds)
+        {
+            float tmp = musicVolumeSlider.value - s.source.volume;
+            s.source.volume = s.source.volume + tmp;
+        }
+    }
+}
