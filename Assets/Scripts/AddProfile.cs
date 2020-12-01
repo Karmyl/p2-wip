2c5e3064b9b51d6841e3eff6e8fc907da1b44b05
Roosa Kontinen <roosa.kontinen@hotmail.com>
2020-12-01 14:08:22 +0200

    Lisatty toiminnallisuus dropboxiin

---
+++ Assets/Scripts/AddProfile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddProfile : MonoBehaviour
+{
+    public InputField input;
+    private int selectedAvatarIndex = 0;
+
+    public void SetSelectedAvatar(int avatarIndex)
+    {
+        selectedAvatarIndex = avatarIndex;
+        Debug.Log(selectedAvatarIndex);
+    }
+
+    public void AddNewProfile()
+    {
+        Debug.Log(input.text);
+        if (input.text.Length > 0 && selectedAvatarIndex != 0)
+        {
+            DatabaseLoader.CreateNewPlayer(input.text, selectedAvatarIndex);
+        }
+        Application.LoadLevel("Profiles");
+
+
+    }
+}
---
+++ Assets/Scripts/PopulateDropdown.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulateDropdown : MonoBehaviour
+{
+    List<Player> players = null;
+    public Dropdown dropdown;
+    private int selectedPlayerIndex = -1;
+  public void Dropdown_IndexChanged(int index)
+    {
+        Debug.Log(index);
+        selectedPlayerIndex = index;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = DatabaseLoader.GetAllPlayers();
+        List<string> names = new List<string>();
+        foreach(Player p in players)
+        {
+            names.Add(p.PlayerName);
+        }
+        dropdown.AddOptions(names);
+    }
+
+    public void ApplyPlayerChange()
+    {
+        if (selectedPlayerIndex != -1)
+        {
+            DatabaseLoader.SetCurrentPlayer(players[selectedPlayerIndex]);
+            Application.LoadLevel("Profiles");
+        }
+    }
+
+}
